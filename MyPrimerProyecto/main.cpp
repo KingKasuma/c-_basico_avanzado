0d8243453f78268a50397e2e56c33300f8e8b45e
Mike <lanselot777hailander@gmail.com>
2018-11-22 16:07:50 -0400

    first commit

---
+++ MyPrimerProyecto/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*SECUENCIAS DE ESCAPE
+    \n -> salto de linea
+    \b -> retroceso
+    \r -> retroceso de carro
+    \t -> tabulador
+    */
+
+    /*DECLARACION DE VARIABLES*/
+    int var, varx;
+    char var2;
+    float var3;
+    double varDoble;
+    /*DECLARACION DE VARIABLES*/
+    edad = 25;
+
+    cout << "Hola 1 " <<"Hola 2"<< endl;
+    return 0;
+}
