9d427362f5c94a2940794ff299396a3d776d2e68
Mike <lanselot777hailander@gmail.com>
2018-11-23 17:02:50 -0400

    Añadiendo archivos sueltos

--- MyPrimerProyecto/main.cpp
+++ MyPrimerProyecto/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+//#define PI 3.14159
 
 using namespace std;
 
@@ -61,6 +63,76 @@ RANGO: -        1.17e-38 a 3.40e38      2.22e-308 a 1.80e308
     cout<<"Ingrese un numero: ";
     cin>>numero;
     cout<<"El valor ingresado es "<<numero<<endl;
+
+    /**OPERADORES MATEMATICOS**/
+    /** Suma -> +
+        Resta -> -
+        Multiplicacion -> *
+        Division -> /
+        MODULO -> %
+    **/
+    int num1 = 7;
+    int num2 = 3;
+    int cociente = num1/num2;
+    int mod = num1%num2;
+    cout<<cociente<<endl;
+    cout<<mod<<endl;
+
+    /**PRECEDENCIA ARITMETICA**/
+    /**1.- Las operaciones entre parentesis **/
+    /**2.- Multiplicacion, division y modulo **/
+    /**3.- Suma y resta **/
+    /** -> **/
+    cout<<14-8/2*3+1<<endl;/**3**/
+    cout<<(14-8)/2*3+1<<endl;/**10**/
+    cout<<14-(8/2*3)+1<<endl;/**3**/
+    cout<<14-8/2*(3+1)<<endl;/**-2**/
+    cout<<(14-8)/2*(3+1)<<endl;/**12**/
+
+    /**OPERADORES LOGICOS**/
+    /**         Y   ->      &&      **/
+    /**         O   ->      ||      **/
+    /**         NO  ->      !      **/
+
+    /**Igualdad ->  ==  **/
+    /**Diferente ->  !=  **/
+    /** Menor ->  <  **/
+    /** Mayor ->  >  **/
+    /**Menor igual ->  <=  **/
+    /**Mayor igual ->  >=  **/
+
+    bool r1 = (true&&true)||false; /**TRUE**/
+    bool r2 = (5==5) || (6!=2) && !(1==1);/**TRUE**/
+    bool r3 = false && true || false; /**FALSE**/
+    bool r4 = (5<3) || (3>=3) || true;/**TRUE**/
+    bool r5 = ((3<=3)&& (5<5) || false);/**FALSE**/
+
+    cout<<r1<<endl;
+    cout<<r2<<endl;
+    cout<<r3<<endl;
+    cout<<r4<<endl;
+    cout<<r5<<endl;
+
+    /**MATH**/
+    double potencia = pow(5,5);/**potencia**/
+    double raiz = sqrt(100.0);/**Raiz cuadrada**/
+    double seno = sin(0.523599);//30
+    double coseno = cos(1.0472);//60
+    double tangente = tan(0.785398);//45
+    cout<<"Potencia :"<<potencia<<endl;
+    cout<<"Raiz :"<<raiz<<endl;
+    cout<<"Seno :"<<seno<<endl;
+    cout<<"Coseno :"<<coseno<<endl;
+    cout<<"Tangente :"<<tangente<<endl;
+
+    /**Constantes
+    definir la constante al principio con la palabra reservada define
+    o tambien con la palabra const dentro del codigo
+    **/
+    const double PI = 3.14159;
+    const double example = 5.898989;
+    cout<<PI<<endl;
+
 
 
     return 0;
