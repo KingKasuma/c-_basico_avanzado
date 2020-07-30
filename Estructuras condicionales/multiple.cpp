23fc24fdb4ad28d8599216d17b3718166f670d72
Mike <lanselot777hailander@gmail.com>
2020-07-30 14:23:56 -0400

    Condicionales

---
+++ Estructuras condicionales/multiple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /**ESTRUCTURA CONDICIONAL MULTIPLE**/
+    int variable;/**char**/
+    cin>>variable;
+    switch(variable){
+        case 1: cout<<"Caso 1"<<endl;
+            break;
+        case 2: cout<<"Caso 1"<<endl;
+            break;
+        case 3: cout<<"Caso 1"<<endl;
+            break;
+        default: cout<<"Ningun caso"<<endl;
+    }
+    return 0;
+}
---
+++ Estructuras condicionales/simple.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /**ESTRUCTUEA CONDICIONAL SIMPLE**/
+    /** if -> si **/
+    /** else -> sino **/
+    if(5<=5){
+        cout<<"Entro al if"<<endl;
+    }else{
+        cout<<"No se cumple el if"<<endl;
+    }
+    return 0;
+}
